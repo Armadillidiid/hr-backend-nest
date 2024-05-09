3cc23c69b7add681800b39de7ed9b2a65968fede
Emmanuel Isenah <emmanuelisenah@gmail.com>
2024-05-09 14:16:35 +0100

    feat: add pino-pretty logger to Fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,20 @@ import {
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({
+      logger: {
+        level: "info",
+        transport: {
+          target: "pino-pretty",
+          options: {
+            translateTime: "HH:MM:ss Z",
+            ignore: "pid,hostname",
+          },
+        },
+      },
+    }),
   );
+  app.enableCors()
   await app.listen(3000, "0.0.0.0");
 }
 bootstrap();
