dca6f4738e05a85e3f801f24510f27603201ab79
Emmanuel Isenah <emmanuelisenah@gmail.com>
2024-05-16 13:39:25 +0100

    fix: update import paths for guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service.js";
 import { AuthController } from "./auth.controller.js";
-import { AuthGuard } from "./guards/auth.guard.access.js";
-import { AuthRefreshGuard } from "./guards/auth.guard.refresh.js";
 import { JwtModule } from "@nestjs/jwt";
+import { AuthGuard } from "./guards/access.guard.js";
+import { AuthRefreshGuard } from "./guards/refresh.guard.js";
 
 @Module({
   imports: [JwtModule.register({})],
