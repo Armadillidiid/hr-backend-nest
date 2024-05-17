f75f188862f594c6ce9a9fb7cd6ec9a94423d833
Emmanuel Isenah <emmanuelisenah@gmail.com>
2024-05-17 11:42:03 +0100

    fix: update broken dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto.js';
+import { z } from 'zod';
+import { createUserSchema } from './create-user.dto.js';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export const updateUserSchema = createUserSchema.partial()
+
+export type UpdateUserDto = z.infer<typeof updateUserSchema>
